6a80075fe12a553c74b7632b3020ee3215c38355
Antti Lahtinen <Anttilahtinen@rakentaja.com>
2021-02-04 13:14:55 +0200

    Yarn scripts fix

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,13 @@
+import { ITeam } from '@antlaht86/types';
+
+export const getNewTeam = (id: ITeam['id'], teamName: ITeam['teamName']) => {
+  const x: ITeam = {
+    id,
+    teamName,
+  };
+  return x;
+};
+
 export const stringIsNotEmptyOrNull = (value: any): value is string => {
   if (typeof value === 'string' && value.trim() !== '') {
     return true;
