7c03fdba4bfe4ddc8f79b5f72eab8b47751ab945
Antti Lahtinen <Anttilahtinen@rakentaja.com>
2021-02-05 12:12:55 +0200

    My-next-app deploy added

--- packages/mylib/src/thing.tsx
+++ packages/mylib/src/thing.tsx
@@ -5,7 +5,7 @@ import { jsx } from "theme-ui";
 export const Thing = () => {
   return (
     <div sx={{ background: theme => theme?.colors.secondary ?? "#895b5b" }}>
-      <h3>Hi!!</h3>
+      <h3>Hi!!!</h3>
       <button sx={{variant: "greenButton"}}>Click me!</button>
     </div>
   );
